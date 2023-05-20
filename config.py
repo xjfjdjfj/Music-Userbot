7c3ae6085639f93d136ab76fd5df589da7bb44ec
xjfjdjfj <132452123+xjfjdjfj@users.noreply.github.com>
2023-05-20 03:37:59 +0330

    Update config.py

--- config.py
+++ config.py
@@ -25,11 +25,11 @@ load_dotenv()
 
 class Config:
     def __init__(self) -> None:
-        self.API_ID: str = os.environ.get("API_ID", None)
-        self.API_HASH: str = os.environ.get("API_HASH", None)
-        self.SESSION: str = os.environ.get("SESSION", None)
+        self.API_ID: str = os.environ.get("API_ID", "11360899")
+        self.API_HASH: str = os.environ.get("API_HASH", "b3a49413dfcaf0a16bd6d15e49772d70")
+        self.SESSION: str = os.environ.get("SESSION", "AQA74U_N4BHd9cnHhJY7ROoJMR4HL_JHDBwuVoKaChd-_I4QrnH0VGN_YOoYNKwrYXDlY35H6tntRWZYtmHRXXbNdkTGzmUEZ_PlLwSFxsx1x8We8Kdw8IBMMRib3xOnwS2BB90Giif6HGgtO2qPpMFIXYeSHe_fbDua1ceW1PktISzYf-jYVaCkiK827-iuHhZJ4DC3ZRCVybgZawbHD-NTEHnIC9smYiBdjNXEDrcfEV3ZQ8UpeOsudNp-y02YuWVbhUMefhOvvWXoO44EBN0ZjmVNLDL18N4UNDaTZFRfqm9-aKvboxuE_Gdpyu9DVO7GyULKhC39LynJ9lE17veSAAAAAT2xSecA")
         self.SUDOERS: list = [
-            int(id) for id in os.environ.get("SUDOERS", " ").split() if id.isnumeric()
+            int(id) for id in os.environ.get("SUDOERS", "5329996263").split() if id.isnumeric()
         ]
         if not self.SESSION or not self.API_ID or not self.API_HASH:
             print("Error: SESSION, API_ID and API_HASH is required!")
